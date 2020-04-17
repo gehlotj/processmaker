6d56bdde6ac130ec163ffe1bab7c698915debf1c
gehlotj <50595086+gehlotj@users.noreply.github.com>
2020-04-17 14:18:59 -0700

    Add files via upload

---
+++ processmaker_download_output_doc.py
@@ -0,0 +1,25 @@
+class Req:
+    def __init__(self,token,url,payload=None):
+        self.token = token
+        self.url = url
+        self.payload = payload
+        self.header = {'Content-Type':'application/json',
+               'Authorization': 'Bearer {}'.format(self.token)}
+
+    def downloadSingleDoc(self,case_num):
+        r = requests.get(self.url,headers=self.header)
+        raw_data = b''
+        if r.status_code == 200:
+            temp = json.loads(r.content.decode())
+            url = self.url+'sysworkflow/en/neoclassic/'+temp[0]['app_doc_link']
+            d = requests.get(url,stream = True)
+            filename = case_num+'.pdf'
+            f = open(filename,'wb')
+            for data in d.iter_content():
+                raw_data = raw_data + data
+                f.write(data)
+            f.close()
+        return send_file(filename,attachment_filename=filename,as_attachment=True)
+
+#use case
+#Req(session['token'],'http://yoururl/api/1.0/workflow/cases/'+appid+'/output-documents').downloadSingleDoc('0000')
